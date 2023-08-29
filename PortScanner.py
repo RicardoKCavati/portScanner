5986be57650cb644761d7a509fe08cbd83cbf946
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2023-08-28 22:20:35 -0300

    Add description on README.md and include PortScanner.py.

---
+++ PortScanner.py
@@ -0,0 +1,52 @@
+import nmap
+from socket import *
+from typing import List
+
+class Scanner:
+    def __init__(self, portNumber):
+        self.canConnect = False
+        self.port = portNumber
+    @property
+    def canConnect(self):
+        return self._canConnect
+    
+    @canConnect.setter
+    def canConnect(self, value):
+        self._canConnect = value
+    
+    @property
+    def port(self):
+        return self._port
+    
+    @port.setter
+    def port(self, value):
+        self._port = value
+
+begin = 75
+end = 80
+target = '127.0.0.1'
+   
+portScanner = nmap.PortScanner() 
+#result = portScanner.scan(target,'1026')
+#result = result['scan'][target]['tcp'][1026]['state']
+
+fiwareScanners = [Scanner('1026'), Scanner("1883"), Scanner("4041"), Scanner("8666"), Scanner("27017")]
+
+for scanner in fiwareScanners:
+    print("testing port: " + scanner.port)
+    result = portScanner.scan(target, scanner.port)
+    result = result['scan'][target]['tcp'][int(scanner.port)]['state']
+    if result == 'open':
+        scanner.canConnect = True
+    else:
+        scanner.canConnect = False
+    
+        
+if all([scanner.canConnect == True for scanner in fiwareScanners]):
+    print("fiware software is installed and running")
+else:
+    print("fiware is not running")
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # portScanner
-Network port scanner
+A python script designed to scan a range of ports on the local machine to validate if a program is installed, in this example, the fiware program was the chosen.
